c7307ac7cfeb6cb4e3b9b574631a63f0799da5ab
SwapnilPande <swapnil.pande98@gmail.com>
2023-04-17 09:13:49 -0400

    Updated IMU test calibration script for latest IMU class spec

--- src/imu_test_calibration.cpp
+++ src/imu_test_calibration.cpp
@@ -1,50 +1,27 @@
 #include <Arduino.h>
 #include "Options.h"
 #include "drive_base.h"
-
-
-
-// Create IMU sensor devices
-Adafruit_LSM6DSOX lsm6ds;
-Adafruit_LIS3MDL lis3mdl;
+#include "imu.h"
 
 // Create a start-up menu with 3 non-default options
-Options options(3);
+Options options(1);
 bool shouldCalibrate;
-bool enableFocStudio;
 
-float offsets[16];
-
-
-byte caldata[68]; // buffer to receive magnetic calibration data
-byte calcount=0;
-
-uint16_t crc16_update(uint16_t crc, uint8_t a)
-{
-  int i;
-  crc ^= a;
-  for (i = 0; i < 8; i++) {
-    if (crc & 1) {
-      crc = (crc >> 1) ^ 0xA001;
-    } else {
-      crc = (crc >> 1);
-    }
-  }
-  return crc;
-}
-
-// Create a drive base object
-// DriveBase drive_base = DriveBase();
-
-// target velocities will be calculated as a percentage of the max velocity
-constexpr float max_velocity_rad_per_sec = 25.0f;
-
+Imu::Imu imu = Imu::Imu();
 void setup()
 {
     // start serial
     Serial.begin(115200);
 
+    // Add options to the menu
+    options.addOption("Calibrate IMU");
+    options.run();
 
+    // Get the selected option
+    int selected_option = options.getSelectedOption();
+    shouldCalibrate = selected_option == 0;
+
+    imu.init(shouldCalibrate);
 
 }
 
@@ -53,38 +30,13 @@ int loopcount = 0;
 void loop()
 {
 
-//   // occasionally print calibration
-//   if (loopcount == 50 || loopcount > 100) {
-//     Serial.print("Cal1:");
-//     for (int i=0; i<3; i++) {
-//       Serial.print(offsets[i], 3);
-//       Serial.print(",");
-//     }
-//     for (int i=3; i<6; i++) {
-//       Serial.print(offsets[i], 3);
-//       Serial.print(",");
-//     }
-//     for (int i=6; i<9; i++) {
-//       Serial.print(offsets[i], 3);
-//       Serial.print(",");
-//     }
-//     Serial.println(offsets[9], 3);
-//     loopcount++;
-//   }
-//   if (loopcount >= 100) {
-//     Serial.print("Cal2:");
-//     Serial.print(offsets[10], 3); Serial.print(",");
-//     Serial.print(offsets[13], 3); Serial.print(",");
-//     Serial.print(offsets[14], 3); Serial.print(",");
-//     Serial.print(offsets[13], 3); Serial.print(",");
-//     Serial.print(offsets[11], 3); Serial.print(",");
-//     Serial.print(offsets[15], 3); Serial.print(",");
-//     Serial.print(offsets[14], 3); Serial.print(",");
-//     Serial.print(offsets[15], 3); Serial.print(",");
-//     Serial.print(offsets[12], 3);
-//     Serial.println();
-//     loopcount = 0;
-//   }
+    imu.loop();
 
-  delay(10);
+    Serial.print("Roll: ");
+    Serial.print(imu.getRoll());
+    Serial.print(" Pitch: ");
+    Serial.print(imu.getPitch());
+    Serial.print(" Yaw: ");
+    Serial.println(imu.getYaw());
+
 }
