0dc073fcfcb33f52448fb0f1ac16786407ecc038
Swapnil Pande <swapnil.pande98@gmail.com>
2023-04-17 09:39:46 -0400

    WIP: Setup main to balance robot

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include "Options.h"
 #include "drive_base.h"
+#include "imu.h"
 
 // Create a start-up menu with 3 non-default options
-Options options(3);
-bool shouldCalibrate;
+Options options(4);
+bool shouldCalibrateMotors;
 bool enableFocStudio;
+bool shouldCalibrateImu;
 
 // Create a drive base object
 DriveBase drive_base = DriveBase();
+
+// Create IMU
+Imu::Imu imu = Imu::Imu();
 
 // target velocities will be calculated as a percentage of the max velocity
 constexpr float max_velocity_rad_per_sec = 25.0f;
@@ -24,10 +29,12 @@ void setup()
     options.addOption("Recalibrate motor + FOCStudio");
     options.run();
     int selected_option = options.getSelectedOption();
-    shouldCalibrate = selected_option == 0 || selected_option == 2;
+    shouldCalibrateMotors = selected_option == 0 || selected_option == 2;
     enableFocStudio = selected_option == 1 || selected_option == 2;
+    shouldCalibrateImu = selected_option == 3;
 
-    drive_base.init(shouldCalibrate, enableFocStudio);
+    drive_base.init(shouldCalibrateMotors, enableFocStudio);
+    imu.init(shouldCalibrateImu);
 
     delay(1000);
 }
@@ -35,4 +42,21 @@ void setup()
 void loop()
 {
     drive_base.loop();
+    imu.loop();
+
+    // Print out the current roll, pitch, and yaw
+    // RPY for the IMU aligns with the robot's frame of reference
+    // Pitch will be the forward/backward tilt
+    Serial.print("Roll: ");
+    Serial.print(imu.getRoll());
+    Serial.print(" Pitch: ");
+    Serial.print(imu.getPitch());
+    Serial.print(" Yaw: ");
+    Serial.println(imu.getYaw());
+
+    // Set motor target using:
+    // drive_base.setTarget(target_left, target_right);
+
+
+
 }
