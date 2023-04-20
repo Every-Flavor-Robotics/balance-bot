8ac0f87e3e575bd12a14d61794b6d9783a9670c6
SwapnilPande <swapnil.pande98@gmail.com>
2023-04-20 13:15:18 -0400

    WIP: robot is balancing

--- include/drive_base.h
+++ include/drive_base.h
@@ -4,18 +4,18 @@
 #define DRIVE_BASE_H
 
 #include <SimpleFOC.h>
-
 // Encoder libs
-#include <sensors/MagneticSensorI2C.h>
-#include "mt6701_sensor.h"
 #include "encoders/calibrated/CalibratedSensor.h"
 #include <Wire.h>
+#include "SPI.h"
+#include "encoders/MT6701/MagneticSensorMT6701SSI.h"
 
 // TODO: These are global because the SimpleFOC commander API doesn't support lambdas to save state in for the callback
 // Need to decide if this is the best solution, or if there is something better we can do
 extern Commander command;
 extern BLDCMotor motor_left;
 extern BLDCMotor motor_right;
+extern SPIClass hspi;
 
 
 class DriveBase {
@@ -31,6 +31,11 @@ public:
     void loop();
 
     void setTarget(float target_left, float target_right);
+
+    void enable();
+
+    float getLeftVelocity();
+    float getRightVelocity();
 
     // void doTargetLeft(char* cmd);
     // void doTargetRight(char* cmd);
@@ -49,9 +54,7 @@ private:
     const int k_left_gpio_wl =  0;
 
     // Right Motor and Encoder pins
-    const int k_right_enc_sda = 15;
-    const int k_right_enc_scl = 32;
-    const int k_right_enc_cs  = 12;
+    const int k_right_enc_cs  = 13;
     const int k_right_gpio_uh = 23;
     const int k_right_gpio_ul = 22;
     const int k_right_gpio_vh = 33;
@@ -62,25 +65,28 @@ private:
     // Motor and Encoder parameters
     const float k_voltage_power_supply = 9.0;
     const float k_voltage_limit = 9.0;
-    const float k_current_limit = 1.0;
+    const float k_current_limit = 10.0;
     const float k_velocity_limit = 100.0;
     const float k_voltage_calibration = 2.0;
 
     bool shouldCalibrate;
     bool enableFocStudio;
 
-    // Encoder, motor, and driver instances
-    MT6701Sensor encoder_left;
-    CalibratedSensor sensor_calibrated_left;
-    BLDCDriver6PWM driver_left;
 
-    MT6701Sensor encoder_right;
+
+    // Encoder, motor, and driver instances
+    // MT6701Sensor encoder_left;
+    MagneticSensorMT6701SSI encoder_left;
+    BLDCDriver6PWM driver_left;
+    CalibratedSensor sensor_calibrated_left;
+
+    MagneticSensorMT6701SSI encoder_right;
     CalibratedSensor sensor_calibrated_right;
     BLDCDriver6PWM driver_right;
 
 
 
-    void initHelper(BLDCMotor& motor, BLDCDriver6PWM& driver, CalibratedSensor& sensor_calibrated, MT6701Sensor& encoder, const char* name);
+    void initHelper(BLDCMotor& motor, BLDCDriver6PWM& driver, CalibratedSensor& sensor_calibrated, MagneticSensorMT6701SSI& encoder, const char* name);
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "imu.h"
 #include "common/pid.h"
 
+
 // Create a start-up menu with 3 non-default options
 Options options(4);
 bool shouldCalibrateMotors;
@@ -11,67 +12,82 @@ bool enableFocStudio;
 bool shouldCalibrateImu;
 
 // Create a drive base object
-DriveBase drive_base = DriveBase();
+DriveBase* drive_base = nullptr;
 
 // Create IMU
 Imu::Imu imu = Imu::Imu();
 
 // Instantiate a PID controller for balancing
-PIDController pid = PIDController(0.3f, 0.001f, 0.0f, 5.0f, 7.0f);
+PIDController pid = PIDController(1.11f, 13.85f, 0.015f, 10000.0f, 20.0f);
 
 // target velocities will be calculated as a percentage of the max velocity
 constexpr float max_velocity_rad_per_sec = 25.0f;
 
 void setup()
 {
+
     // start serial
     Serial.begin(115200);
+
 
     // Add options and run menu before setting up motor
     options.addOption("Recalibrate motor");
     options.addOption("FOCStudio");
     options.addOption("Recalibrate motor + FOCStudio");
+    options.addOption("Calibrate IMU");
     options.run();
+
+    drive_base = new DriveBase();
+
     int selected_option = options.getSelectedOption();
     shouldCalibrateMotors = selected_option == 0 || selected_option == 2;
     enableFocStudio = selected_option == 1 || selected_option == 2;
     shouldCalibrateImu = selected_option == 3;
-
-    drive_base.init(shouldCalibrateMotors, enableFocStudio);
-    imu.init(shouldCalibrateImu);
+    drive_base->init(shouldCalibrateMotors, enableFocStudio);
+    imu.init(false);
 
     delay(1000);
+    drive_base->enable();
+
+    Serial.println("Letting IMU stabilize...");
+    for (int i = 0; i < 1000; i++)
+    {
+        imu.loop();
+    }
 }
 
 void loop()
 {
-    drive_base.loop();
+    // Start time
+    unsigned long start_time = micros();
+
+    drive_base->loop();
     imu.loop();
 
     // Compute error
     // For balancing alone, we only need the pitch angle
     float error = imu.getPitch();
 
-    float command = pid.operator(error);
+    float command = pid(error);
 
-    drive_base.setTarget(command, command);
+    drive_base->setTarget(command, command);
 
-
-    // Print out the current roll, pitch, and yaw
-    // RPY for the IMU aligns with the robot's frame of reference
-    // Pitch will be the forward/backward tilt
-    // Serial.print("Roll: ");
-    // Serial.print(imu.getRoll());
-    Serial.print(" Pitch: ");
-    Serial.print(imu.getPitch());
-
-    Serial.print(" Command: ");
-    Serial.print(command);
-    // Serial.print(" Yaw: ");
-    // Serial.println(imu.getYaw());
+    // Print loop time
+    // Serial.print("Loop time: ");
+    // Serial.print(micros() - start_time);
 
 
-
-
-
+    // // Print out the current roll, pitch, and yaw
+    // // RPY for the IMU aligns with the robot's frame of reference
+    // // Pitch will be the forward/backward tilt
+    // // Serial.print("Roll: ");
+    // // Serial.print(imu.getRoll());
+    // // Serial.print(" Yaw: ");
+    // // Serial.println(imu.getYaw());
+    // Serial.print(" Pitch: ");
+    // Serial.println(imu.getPitch());
+    // Serial.print(" Command: ");
+    // Serial.print(command);
+    // Serial.print(" Velocity: ");
+    // Serial.println(drive_base -> getLeftVelocity());
 }
