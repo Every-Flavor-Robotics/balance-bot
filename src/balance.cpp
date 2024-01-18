cd3d048005e4341ef6dd3ab2e4880017dded2442
Swapnil Pande <swapnil.pande98@gmail.com>
2024-01-18 11:38:01 -0500

    Balance bot is balancing and controllable with joystick

--- src/balance.cpp
+++ src/balance.cpp
@@ -35,7 +35,7 @@ DriveBase *drive_base = nullptr;
 // Create IMU
 Imu::Imu imu = Imu::Imu();
 // low pass
-LowPassFilter velocity_command = LowPassFilter(0.6f);
+LowPassFilter velocity_command = LowPassFilter(0.1f);
 
 LowPassFilter is_static_detector = LowPassFilter(1.0f);
 
@@ -44,19 +44,19 @@ LowPassFilter is_static_detector = LowPassFilter(1.0f);
 // float k_2 = 23.84869906f;
 // float k_3 = 14.1f;
 
-// float k_0 = 0.03535534f;
-// float k_1 = 3.15890315f;
-// float k_2 = 24.29259086f;
-// float k_3 = 14.2874019f;
+float k_0 = 0.03535534f;
+float k_1 = 3.15890315f;
+float k_2 = 24.29259086f;
+float k_3 = 14.2874019f;
 
 float standing_k_0 = 0.02536067977521028;
 float standing_k_1 = 3.308069289823837;
 float standing_k_2 = 25.39465377043689;
 float standing_k_3 = 15.20592074007437;
-float k_0 = 0.040824829046041344;
-float k_1 = 3.581318765549753;
-float k_2 = 28.127196312812885;
-float k_3 = 17.311472756698436;
+// float k_0 = 0.040824829046041344;
+// float k_1 = 3.581318765549753;
+// float k_2 = 28.127196312812885;
+// float k_3 = 17.311472756698436;
 
 bool pos_reset = false;
 
@@ -68,17 +68,17 @@ float desired_yaw = 0.0f;
 
 float angle_offset = -0.003;
 
-float target_freq = 250.0f;
-
 float desired_pos = 0;
 float desired_vel = 0;
-const float MAX_VEL = 8.0f;
 float desired_steer_rate = 0;
+bool is_static = true;
+
 // Radians per second
-const float MAX_STEER_RATE = 1.7f;
+const float MAX_VEL = 4.0f;
+const float MAX_STEER_RATE = 1.3f;
 
 // Steering pid controller
-PIDController steering_pid = PIDController(3.5f, 0.0000f, 0.1f, 10000, 10000);
+PIDController steering_pid = PIDController(3.7f, 0.0000f, 0.05f, 10000, 10000);
 
 // Joystick data struct
 // uint8_t broadcast_address[] = {0x24, 0x0A, 0xC4, 0x00, 0x4C, 0x6E};
@@ -106,8 +106,8 @@ void on_joystick_data_receive(const uint8_t *mac, const uint8_t *incoming_data,
 
 void loop_foc(void *pvParameters);
 
-unsigned long loop_time = 0;
-double hz = 0;
+unsigned long last_time = 0;
+float loop_time = 0;
 int samples = 0;
 unsigned long last_print_time = 0;
 void setup()
@@ -117,22 +117,13 @@ void setup()
 
   delay(3000);
 
-  // Add options and run menu before setting up motor
-  options.addOption("Recalibrate motor");
-  options.addOption("FOCStudio");
-  options.addOption("Recalibrate motor + FOCStudio");
-  options.addOption("Calibrate IMU");
-  options.run();
-
   drive_base = new DriveBase();
 
-  int selected_option = options.getSelectedOption();
+  //   int selected_option = options.getSelectedOption();
 
-  should_calibrate_motors = selected_option == 0 || selected_option == 2;
-  enable_foc_studio = selected_option == 1 || selected_option == 2;
-  should_calibrate_imu = selected_option == 3;
-
-  drive_base->init(false, enable_foc_studio);
+  bool calibrate_motors = false;
+  bool enable_foc_studio = false;
+  drive_base->init(calibrate_motors, enable_foc_studio);
   imu.init(false);
 
   delay(1000);
@@ -182,6 +173,8 @@ void setup()
       1);             /* pin task to core 1 */
 
   drive_base->enable();
+
+  last_time = micros();
 }
 
 void loop_foc(void *pvParameters)
@@ -195,20 +188,6 @@ void loop_foc(void *pvParameters)
 
     //   Print loop frequency
     esp_task_wdt_reset();
-
-    hz += 1000000.0f / (micros() - loop_time);
-    samples += 1;
-
-    loop_time = micros();
-
-    if (millis() - last_print_time > 1000)
-    {
-      //   Serial.print("Hz: ");
-      //   Serial.println(hz / samples);
-      hz = 0;
-      samples = 0;
-      last_print_time = millis();
-    }
   }
 }
 
@@ -242,66 +221,47 @@ void loop()
                (drive_base->get_right_position() - zero_wheel_right)) /
               2;
 
-  desired_yaw += desired_steer_rate * 0.004f;
+  // Get loop time in seconds
+  loop_time = (micros() - last_time) / 1000000.0f;
+  last_time = micros();
+  desired_yaw += desired_steer_rate * loop_time;
+  desired_pos += desired_vel * loop_time;
 
   float filtered_velocity = is_static_detector(abs(wheel_velocity));
 
   // u = -kx
   float command;
   float command_steer;
-  if (desired_vel > 0.05f || desired_vel < -0.05f)
-  {
-    desired_pos = wheel_position + pitch + desired_vel * 0.004f;
 
-    command = -(k_0 * (wheel_position - pitch - desired_pos) +
-                k_1 * (wheel_velocity - pitch_rate - desired_vel) +
-                k_2 * pitch + k_3 * pitch_rate);
-    command_steer = -steering_pid(yaw - zero_yaw - desired_yaw);
+  // Compute steering command
+  float steer_error = yaw - zero_yaw - desired_yaw;
+  command_steer = -steering_pid(steer_error);
+
+  command = -0.85 * (k_0 * (wheel_position + pitch - desired_pos) +
+                     k_1 * (wheel_velocity + pitch_rate - desired_vel) +
+                     k_2 * pitch + k_3 * pitch_rate);
+  if (desired_vel < 0.05f && !pos_reset && desired_steer_rate == 0.0f)
+  {
+    desired_pos = wheel_position + pitch;
+    desired_yaw = yaw - zero_yaw;
+    pos_reset = true;
+  }
+  else if (desired_vel > 0.05f || desired_steer_rate > 0.05f)
+  {
+    pos_reset = false;
+  }
+
+  drive_base->set_target(-command + command_steer, command + command_steer);
+
+  // If the robot has fallen, disable the motors
+  if (abs(pitch) > 0.8f)
+  {
+    drive_base->disable();
   }
   else
   {
-    float steer_error = yaw - zero_yaw - desired_yaw;
-    // Once the robot stops oscillating, reset the desired position
-    if (filtered_velocity < 0.05f && !pos_reset)
-    {
-      desired_pos = wheel_position;
-      pos_reset = true;
-    }
-    else if (filtered_velocity > 0.05f || abs(steer_error) > 0.1f)
-    {
-      pos_reset = false;
-    }
-
-    command = -0.90 * (standing_k_0 * (wheel_position + pitch - desired_pos) +
-                       standing_k_1 * (wheel_velocity + pitch_rate) +
-                       standing_k_2 * pitch + standing_k_3 * pitch_rate);
-    command_steer = -steering_pid(steer_error);
+    drive_base->enable();
   }
-
-  //   String str = String(command) + "\t" + String(command_steer) + "\t" +
-  //                String(pitch) + "\t" + String(pitch_rate);
-
-  //   freq_println(str, 50);
-
-  drive_base->set_target(-command - command_steer, command + command_steer);
-
-  //   if (abs(pitch) > 0.6f || abs(wheel_velocity) > 8)
-  //   {
-  //     drive_base->disable();
-  //   }
-  //   else
-  //   {
-  //     drive_base->enable();
-  //   }
-
-  //   // Sleep to maintain target frequency
-  //   unsigned long end_time = micros();
-  //   unsigned long elapsed_time = end_time - start_time;
-  //   unsigned long target_time = (unsigned long)(1000000.0f / target_freq);
-  //   if (elapsed_time < target_time)
-  //   {
-  //     delayMicroseconds(target_time - elapsed_time);
-  //   }
 
   vTaskDelay(1 / portTICK_PERIOD_MS);
 }
@@ -316,6 +276,24 @@ void on_joystick_data_receive(const uint8_t *mac, const uint8_t *incoming_data,
   //   Map joy1_x to desired_vel
   //   Map joy2_y to desired_steer_rate
 
-  desired_vel = MAX_VEL * ((float)data.joy1_y / 100.0f);
+  //   Deadband
+  if (abs(data.joy1_x) < 10)
+  {
+    data.joy1_x = 0;
+  }
+  else
+  {
+    is_static = false;
+  }
+  if (abs(data.joy2_x) < 10)
+  {
+    data.joy2_x = 0;
+  }
+  else
+  {
+    is_static = false;
+  }
+
+  desired_vel = velocity_command(MAX_VEL * ((float)data.joy1_y / 100.0f));
   desired_steer_rate = -MAX_STEER_RATE * ((float)data.joy2_x / 100.0f);
 }
